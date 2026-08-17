65bd40fb14a37fc52cc9c5f4078185fd4510cf29
tandpapps <tandpapps01@gmail.com>
2026-08-17 15:51:36 +0300

    Add route-based reporting entrypoints

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { HrReporting, LeaveReporting } from "./Reporting";
 import "./styles.css";
+
+function Root() {
+  const path = window.location.pathname.replace(/\/+$/, "") || "/";
+
+  if (path === "/hr-reporting") return <HrReporting />;
+  if (path === "/leave-reporting") return <LeaveReporting />;
+  return <App />;
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
